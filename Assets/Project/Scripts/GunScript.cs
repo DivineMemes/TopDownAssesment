9f8ef2c41d5224aedb61e5bd3bfd9fc23e417262
Wyatt Gallagher <s179053@students.aie.edu.au>
2018-04-18 10:21:08 -0700

    initial commit

---
+++ Assets/Project/Scripts/GunScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    Vector3 mousePos;
+
+    void FixedUpdate()
+    {
+        LookTowardsMouse();
+    }
+    void LookTowardsMouse()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z - transform.position.z));
+        mousePos.y = transform.position.y;
+        // int mousex =
+        transform.LookAt(mousePos);
+        Debug.DrawLine(transform.position, Input.mousePosition);
+    }
+
+}
